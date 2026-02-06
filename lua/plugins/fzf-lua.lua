9a4e6f222488c9f1b2e39e8bfd8972586e5390bb
Kevin Sladowski <ksladowski@mailbox.org>
2026-02-06 09:34:09 -0600

    added snacks

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -8,14 +8,14 @@ return {
         "telescope",
     },
     ---@diagnostic enable: missing-fields
-    keys = {
-        { "<leader>:", "<cmd>FzfLua command_history<cr>", desc = "[:] Command History" },
-        { "<leader>f", "<cmd>FzfLua files<cr>", desc = "Find [f]iles" },
-        { "<leader>b", "<cmd>FzfLua buffers<cr>", desc = "Find [b]uffers" },
-        { "<leader>s", "<cmd>FzfLua live_grep_native<cr>", desc = "[s]earch cwd with rg" },
-        { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "[/] Grep Current Buffer" },
-        { "<leader>hm", "<cmd>FzfLua manpages<cr>", desc = "Help [m]an Pages" },
-        { "<leader>hn", "<cmd>FzfLua helptags<cr>", desc = "Help [n]eovim" },
-        { "<leader>hk", "<cmd>FzfLua keymaps<cr>", desc = "Help [k]eybindings" },
-    },
+    -- keys = {
+    --     { "<leader>:", "<cmd>FzfLua command_history<cr>", desc = "[:] Command History" },
+    --     { "<leader>f", "<cmd>FzfLua files<cr>", desc = "Find [f]iles" },
+    --     { "<leader>b", "<cmd>FzfLua buffers<cr>", desc = "Find [b]uffers" },
+    --     { "<leader>s", "<cmd>FzfLua live_grep_native<cr>", desc = "[s]earch cwd with rg" },
+    --     { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "[/] Grep Current Buffer" },
+    --     { "<leader>hm", "<cmd>FzfLua manpages<cr>", desc = "Help [m]an Pages" },
+    --     { "<leader>hn", "<cmd>FzfLua helptags<cr>", desc = "Help [n]eovim" },
+    --     { "<leader>hk", "<cmd>FzfLua keymaps<cr>", desc = "Help [k]eybindings" },
+    -- },
 }
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,59 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        image = { enabled = true },
+        picker = { enabled = true },
+        explorer = { enabled = true },
+        indent = { enabled = true },
+        dim = { enabled = true },
+        keymap = { enabled = true },
+        input = { enabled = true },
+    },
+    keys = {
+        {
+            "<leader><space>",
+            function()
+                Snacks.picker.smart()
+            end,
+            desc = "Smart Find Files",
+        },
+        {
+            "<leader>,",
+            function()
+                Snacks.picker.buffers()
+            end,
+            desc = "Buffers",
+        },
+        {
+            "<leader>/",
+            function()
+                Snacks.picker.grep()
+            end,
+            desc = "Grep",
+        },
+        {
+            "<leader>:",
+            function()
+                Snacks.picker.command_history()
+            end,
+            desc = "Command History",
+        },
+        {
+            "<leader>n",
+            function()
+                Snacks.picker.notifications()
+            end,
+            desc = "Notification History",
+        },
+        {
+            "<leader>e",
+            function()
+                Snacks.explorer()
+            end,
+            desc = "File Explorer",
+        },
+    },
+}
